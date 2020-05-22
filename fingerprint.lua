88958495000094cc4619d5a0eec6ba0bdd2300b2
GlorifiedPig <killingpigs123@gmail.com>
2020-05-22 12:00:34 +0200

    check if folder exists

--- fingerprint.lua
+++ fingerprint.lua
@@ -1,4 +1,14 @@
 
 print( "Please input a folder name to convert." )
+
 local folderName = io.read()
-print(folderName)
+
+local function folderExists( file )
+    local ok, err, code = os.rename(file, file)
+    if not ok and code == 13 then
+        return true
+    end
+    return ok, err
+end
+
+if not folderExists( folderName ) then print( "Folder does not exist." ) return end
