ef51b9b550f17c69dd451a43c9821aa7de5cab65
GlorifiedPig <killingpigs123@gmail.com>
2020-06-11 21:34:44 +0200

    less hardcoded

--- fingerprint.py
+++ fingerprint.py
@@ -1,23 +1,24 @@
 import io, os, random
 
-def append_random_line( file_name, text ):
-    with open( file_name ) as file: # Opens file
+def append_line( fileName, line, text ):
+    line = line + 1
+    with open( fileName ) as file:
         lines = file.readlines()
-
-        # Appends a new line to a random line
-        randomInt = random.choice(range( 0, len( lines ) ) )
-        
-        line = str( lines[ randomInt ] )
-        lines[ int( randomInt ) ] = line + text
-
-        # Saves the changes
-        out = open( file_name, 'w' )
+        oldLine = lines[line]
+        lines[line] = oldLine + text
+        out = open( fileName, "w" )
         out.writelines( lines )
-        out.close() # Closes the file
+        out.close()
+
+def append_random_line( fileName, text ):
+    with open( fileName ) as file:
+        lines = file.readlines()
+        randomInt = random.choice( range( 1, len( lines ) - 1 ) )
+        append_line( fileName, randomInt, text )
 
 # take biggest line in ALL files under folders
 # add -- {{ user.id }}
 
 print( "Running fingerprinter..." )
 
-append_random_line( 'test.lua', '-- {user.id}' )
+append_random_line( "test.lua", "-- {user.id}" )
