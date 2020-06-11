28f6e2d2f618f66b7d3f8eb6e1f540d3f8861964
GlorifiedPig <killingpigs123@gmail.com>
2020-06-11 22:10:20 +0200

    very epic !

--- fingerprint.py
+++ fingerprint.py
@@ -15,18 +15,30 @@ def append_line( fileName, line, text ):
     out.writelines( lines )
     out.close()
 
-def append_line_usersha( fileName, line ):
+def append_line_sha( fileName, line ):
     sha_randomkey = randomString()
     changed_lines.append( "Line " + str( line ) + ": SHA256 Encrypted, Key '" + sha_randomkey + "'" )
     append_line( fileName, line, " -- {{ user_id sha256 " + sha_randomkey + " }}" )
 
-def append_random_line_usersha( fileName ):
+def append_line_xor( fileName, line ):
+    xor_randomkey = str( random.choice( range( 10, 100000 ) ) )
+    changed_lines.append( "Line " + str( line ) + ": XOR Encrypted, Key '" + xor_randomkey + "'" )
+    append_line( fileName, line, " -- {{ user_id | " + xor_randomkey + " }}" )
+
+def append_random_line_sha( fileName ):
     with open( fileName, 'r' ) as file:
         lines = file.readlines()
         randomInt = random.choice( range( 1, len( lines ) ) )
-        append_line_usersha( fileName, randomInt )
+        append_line_sha( fileName, randomInt )
+
+def append_random_line_xor( fileName ):
+    with open( fileName, 'r' ) as file:
+        lines = file.readlines()
+        randomInt = random.choice( range( 1, len( lines ) ) )
+        append_line_xor( fileName, randomInt )
 
 print( "Running fingerprinter..." )
 
-append_random_line_usersha( "test.lua" )
+append_random_line_sha( "test.lua" )
+append_random_line_xor( "test.lua" )
 print(changed_lines)
