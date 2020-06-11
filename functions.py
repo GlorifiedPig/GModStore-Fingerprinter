6bc96fafe4eed4c1294295496bed633c03cc90d4
Mel-vin <melvin.calis2@gmail.com>
2020-06-11 22:16:30 +0200

    ratard

---
+++ functions.py
@@ -0,0 +1,35 @@
+import io, os, random, string
+
+def randomString( stringLength = 8 ):
+    letters = string.ascii_lowercase
+    return ''.join( random.choice( letters ) for i in range( stringLength ) )
+
+def append_line( fileName, line, text ):
+    line = line - 1
+    lines = open( fileName, 'r' ).readlines()
+    lines[line] = str( lines[line] )[:-1] + text + "\n"
+    out = open( fileName, 'w' )
+    out.writelines( lines )
+    out.close()
+
+def append_line_sha( fileName, line ):
+    sha_randomkey = randomString()
+    
+    append_line( fileName, line, " -- {{ user_id sha256 " + sha_randomkey + " }}" )
+
+def append_line_xor( fileName, line ):
+    xor_randomkey = str( random.choice( range( 10, 100000 ) ) )
+    
+    append_line( fileName, line, " -- {{ user_id | " + xor_randomkey + " }}" )
+
+def append_random_line_sha( fileName ):
+    with open( fileName, 'r' ) as file:
+        lines = file.readlines()
+        randomInt = random.choice( range( 1, len( lines ) ) )
+        append_line_sha( fileName, randomInt )
+
+def append_random_line_xor( fileName ):
+    with open( fileName, 'r' ) as file:
+        lines = file.readlines()
+        randomInt = random.choice( range( 1, len( lines ) ) )
+        append_line_xor( fileName, randomInt )
